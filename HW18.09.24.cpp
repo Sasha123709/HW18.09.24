79d006cf98a7ed1ff854418ed712c17b429ad409
SashkoSt <oleksandrstefaniyk@gmail.com>
2024-09-17 17:55:52 +0300

    Add project files.

---
+++ HW18.09.24.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void checkNumber(int& num) {
+    if (num > 0) {
+        cout << "Число додатне." << endl;
+    }
+    else if (num < 0) {
+        cout << "Число від'ємне." << endl;
+    }
+    else {
+        cout << "Число дорівнює нулю." << endl;
+    }
+}
+
+int main() {
+    int number;
+
+
+    cout << "Введіть число: ";
+    cin >> number;
+
+
+    checkNumber(number);
+
+    return 0;
+}
