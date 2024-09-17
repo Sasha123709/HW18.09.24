bd0122124b86ff687927a40188ffbeda63ba1516
SashkoSt <oleksandrstefaniyk@gmail.com>
2024-09-17 18:04:30 +0300

    Завдання 2

--- HW18.09.24.cpp
+++ HW18.09.24.cpp
@@ -1,30 +1,84 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//
+//void checkNumber(int& num) {
+//    if (num > 0) {
+//        cout << "Число додатне." << endl;
+//    }
+//    else if (num < 0) {
+//        cout << "Число від'ємне." << endl;
+//    }
+//    else {
+//        cout << "Число дорівнює нулю." << endl;
+//    }
+//}
+//
+//int main() {
+//    SetConsoleCP(1251); 
+//    SetConsoleOutputCP(1251);
+//    int number;
+//
+//
+//    cout << "Введіть число: ";
+//    cin >> number;
+//
+//
+//    checkNumber(number);
+//
+//    return 0;
+//}
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>  
 #include <Windows.h>
 using namespace std;
 
-void checkNumber(int& num) {
-    if (num > 0) {
-        cout << "Число додатне." << endl;
-    }
-    else if (num < 0) {
-        cout << "Число від'ємне." << endl;
-    }
-    else {
-        cout << "Число дорівнює нулю." << endl;
+
+void fillArray(double* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        arr[i] = (rand() % 10000) / 100.0; 
     }
 }
 
+
+void showArray(double* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-    SetConsoleCP(1251); 
+    srand(time(0));
+    char choice;
+    SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int number;
+    do {
+        int size;
+        cout << "Введіть розмір масиву: ";
+        cin >> size;
 
+        
+        double* array = new double[size];
 
-    cout << "Введіть число: ";
-    cin >> number;
+        
+        fillArray(array, size);
 
+        
+        cout << "Ваш масив: ";
+        showArray(array, size);
 
-    checkNumber(number);
+        
+        delete[] array;
 
+        
+        cout << "Бажаєте створити новий масив? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Програма завершена." << endl;
     return 0;
 }
