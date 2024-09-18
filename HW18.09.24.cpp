51e82ab678f8f30bf450063bd868cb0526cba332
SashkoSt <oleksandrstefaniyk@gmail.com>
2024-09-18 15:29:46 +0300

    Завдання 4

--- HW18.09.24.cpp
+++ HW18.09.24.cpp
@@ -83,55 +83,137 @@
 //    return 0;
 //}
 
+//#include <iostream>
+//#include <cstdlib>  
+//#include <ctime>    
+//#include <Windows.h>
+//
+//void printArray(int* array, int size) {
+//    for (int i = 0; i < size; ++i) {
+//        std::cout << array[i] << " ";
+//    }
+//    std::cout << std::endl;
+//}
+//
+//
+//void swapEvenOdd(int* array, int size) {
+//    for (int i = 1; i < size; i += 2) {
+//        if (i + 1 < size) {
+//            std::swap(array[i - 1], array[i]);
+//        }
+//    }
+//}
+//
+//int main() {
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//    std::srand(std::time(nullptr)); 
+//
+//    int size;
+//    std::cout << "Введіть розмір масиву: ";
+//    std::cin >> size;
+//
+//
+//    int* array = new int[size];
+//
+//    
+//    for (int i = 0; i < size; ++i) {
+//        array[i] = std::rand() % 100;
+//    }
+//
+//    std::cout << "Початковий масив: ";
+//    printArray(array, size);
+//
+//
+//    swapEvenOdd(array, size);
+//
+//    std::cout << "Масив після обміну: ";
+//    printArray(array, size);
+//
+//
+//    delete[] array;
+//
+//    return 0;
+//}
+
 #include <iostream>
-#include <cstdlib>  
-#include <ctime>    
 #include <Windows.h>
 
-void printArray(int* array, int size) {
+void separatePosNeg(int* array, int size, int*& positives, int& posCount, int*& negatives, int& negCount) {
+    
+    posCount = 0;
+    negCount = 0;
+    for (int i = 0; i < size; ++i) {
+        if (array[i] > 0) {
+            ++posCount;
+        }
+        else if (array[i] < 0) {
+            ++negCount;
+        }
+    }
+
+    positives = new int[posCount];
+    negatives = new int[negCount];
+
+
+    int posIndex = 0;
+    int negIndex = 0;
+    for (int i = 0; i < size; ++i) {
+        if (array[i] > 0) {
+            positives[posIndex++] = array[i];
+        }
+        else if (array[i] < 0) {
+            negatives[negIndex++] = array[i];
+        }
+    }
+}
+
+
+void printArray(int* array, int size, const char* arrayName) {
+    std::cout << arrayName << ": ";
     for (int i = 0; i < size; ++i) {
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
 }
 
-
-void swapEvenOdd(int* array, int size) {
-    for (int i = 1; i < size; i += 2) {
-        if (i + 1 < size) {
-            std::swap(array[i - 1], array[i]);
-        }
-    }
-}
-
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::srand(std::time(nullptr)); 
-
-    int size;
-    std::cout << "Введіть розмір масиву: ";
-    std::cin >> size;
+    const int size = 10; 
+    int array[size];
 
 
-    int* array = new int[size];
-
-    
+    std::cout << "Введіть " << size << " цілих чисел: ";
     for (int i = 0; i < size; ++i) {
-        array[i] = std::rand() % 100;
+        std::cin >> array[i];
     }
 
-    std::cout << "Початковий масив: ";
-    printArray(array, size);
+    
+    int* positives = nullptr;
+    int* negatives = nullptr;
+    int posCount = 0, negCount = 0;
 
 
-    swapEvenOdd(array, size);
-
-    std::cout << "Масив після обміну: ";
-    printArray(array, size);
+    separatePosNeg(array, size, positives, posCount, negatives, negCount);
 
 
-    delete[] array;
+    if (posCount > 0) {
+        printArray(positives, posCount, "Додатні числа");
+    }
+    else {
+        std::cout << "Додатніх чисел немає.\n";
+    }
+
+    if (negCount > 0) {
+        printArray(negatives, negCount, "Від'ємні числа");
+    }
+    else {
+        std::cout << "Від'ємних чисел немає.\n";
+    }
+
+    delete[] positives;
+    delete[] negatives;
 
     return 0;
 }
